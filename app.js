e0631467522d4f784e530308d5d8d96247ec3348
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2020-01-26 01:57:35 -0500

    added utils

---
+++ app.js
@@ -0,0 +1,26 @@
+const config = require('./utils/config.js')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const cors = require('cors')
+const blogRouter = require('./controllers/blogBook.js')
+const middleware = require('./utils/middleware')
+const mongoose = require('mongoose')
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
+
+  app.use(cors())
+  app.use(bodyParser.json())
+  app.use(middleware.requestLogger)
+  app.use('/api/blogs', blogRouter)
+  
+  app.use(middleware.unknownEndpoint)
+  app.use(middleware.errorHandler)
+
+module.exports = app
